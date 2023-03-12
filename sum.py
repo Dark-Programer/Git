9131c0700aca395faf233f990c44e7aca3f18b30
Arka Chakraborty <arkachakrabortythedark006@gmail.com>
2023-03-12 10:20:39 +0530

    initial commit

---
+++ sum.py
@@ -0,0 +1,9 @@
+# First step: git init then git will give you a specific location
+# then ls -a to check git file is created or not
+
+var1=5
+var2=5
+sum=int(var1+var2)
+print(sum)
+
+# In git first we add file then we will track the file
